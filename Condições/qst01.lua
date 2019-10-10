3e7ff8bc034766346b5c8e651ce1184842dab5ec
Jonas Jakiminski <jonassilva2099@gmail.com>
2019-10-10 01:46:48 -0300

    Update qst01.lua

--- Condições/qst01.lua
+++ Condições/qst01.lua
@@ -1,1 +1,15 @@
-
+print("Informe Peso e Altura.")
+altura, peso = io.read("*n","*n")
+imc = peso / (altura*altura)
+print(imc)
+msg = ""
+if imc<=18 then
+    msg = "Abaixo do Peso"
+elseif imc >18 and imc<25 then
+    msg = "Peso Normal"
+elseif imc>=25 and imc<30 then
+    msg = "Sobrepeso"
+else
+    msg = "Obeso Mórbido"
+end
+print(msg)
