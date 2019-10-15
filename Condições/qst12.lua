5712c506f53dbd6496572058727af7da8819937c
JadielJr <39445244+JadielJr@users.noreply.github.com>
2019-10-15 15:10:53 -0300

    Update qst12.lua

--- Condições/qst12.lua
+++ Condições/qst12.lua
@@ -1,1 +1,11 @@
+n1 = io.read("*n")
+n2 = io.read("*n")
+n3 = io.read("*n")
 
+if n1>n2 and n1>n3 then
+    print(n1.." eh o maior")
+elseif n2>n1 and n2>n3 then
+    print(n2.." eh o maior")
+else
+    print(n3.." eh maior")
+end
