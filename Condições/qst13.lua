5b561afe5d5e7751a477f75749ce1c871c592247
JadielJr <39445244+JadielJr@users.noreply.github.com>
2019-10-15 15:20:07 -0300

    Create qst13.lua

---
+++ Condições/qst13.lua
@@ -0,0 +1,19 @@
+n1 = io.read("*n")
+n2 = io.read("*n")
+n3 = io.read("*n")
+
+if n1~=1 and n2~=1 and n3~=1 then
+    print("1")
+end
+if n1~=2 and n2~=2 and n3~=2 then
+    print("2")
+end
+if n1~=3 and n2~=3 and n3~=3 then
+    print("3")
+end
+if n1~=4 and n2~=4 and n3~=4 then
+    print("4")
+end
+if n1~=5 and n2~=5 and n3~=5 then
+    print("5")
+end
